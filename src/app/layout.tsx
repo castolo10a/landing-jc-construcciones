c6cf4e64afb4db849e7c03ca9e7bd4d240eed82f
castolo10a <castillod159@gmail.com>
2025-07-03 22:34:23 -0500

    feat: add toaster to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Footer, Header } from "@/components";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Toaster position="bottom-right" reverseOrder={false} />
         <Footer />
       </body>
     </html>
